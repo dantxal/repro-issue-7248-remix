2ce21575aab13851785cda3eb63b36f55b451db0
dantxal <dantxal@gmail.com>
2023-08-25 14:53:13 -0300

    Setup minimum repro for issue

--- app/root.tsx
+++ app/root.tsx
@@ -1,39 +1,31 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction, LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
+import { OverlayProvider } from "react-aria";
 
-import { getUser } from "~/session.server";
-import stylesheet from "~/tailwind.css";
+import styles from "./tailwind.css";
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
-
-export const loader = async ({ request }: LoaderArgs) => {
-  return json({ user: await getUser(request) });
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
 };
 
 export default function App() {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full bg-neutral-100">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
+
       <body className="h-full">
-        <Outlet />
-        <ScrollRestoration />
+        {/* If we comment out the OverlayProvider tags, HMR works */}
+        <OverlayProvider className="h-full">
+          <Outlet />
+        </OverlayProvider>
         <Scripts />
         <LiveReload />
       </body>
